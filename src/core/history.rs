f2c5d704b7824a6daa8417a82de8e3e5ba003761
Paul Lewis <paularthurlewis@gmail.com>
2023-04-01 10:11:25 -0700

    updates to history

--- src/core/history.rs
+++ src/core/history.rs
@@ -8,29 +8,48 @@ pub struct History<S : State, A : Action> {
     nodes: Vec<HistoryNode<S, A>>,
 }
 
-pub struct HistoryNode<S : State, A : Action> {
-    pub state: S,
-    pub actions: HashMap<usize, A>,
+/// A history node can be a state or set of actions.
+/// The history alternates between states and actions.
+pub enum HistoryNode<S : State, A : Action> {
+    State(S),
+    Actions(HashMap<usize, A>),
 }
 
 impl <S : State, A : Action> History<S, A> {
     /// Create a new history with an initial state.
     pub fn new(initial_state: S) -> Self {
         History {
-            nodes: vec![HistoryNode { state: initial_state, actions: HashMap::new() }],
+            nodes: vec![HistoryNode::State(initial_state)],
         }
-    }
-
-    /// Clear the history.
-    pub fn clear(&mut self) {
-        self.nodes.clear();
     }
 
     /// Add the next state and the actions taken by each agent
     /// to arrive at that state.
-    /// @param state the current state
-    /// @param actions the actions taken by each player to end up in the current state
-    pub fn add(&mut self, state: S, actions: HashMap<usize, A>) {
-        self.nodes.push(HistoryNode { state, actions });
+    pub fn push(&mut self, state: S, actions: HashMap<usize, A>) {
+        self.nodes.push(HistoryNode::Actions(actions));
+        self.nodes.push(HistoryNode::State(state));
+    }
+
+    /// Remove the last state and actions from the history.
+    /// If the history is already at an initial state, then
+    /// this function will panic.
+    pub fn pop(&mut self) {
+        if self.nodes.len() < 2 {
+            panic!("Cannot remove last state from history");
+        }
+        self.nodes.pop();
+        self.nodes.pop();
+    }
+
+    pub fn peek(&self) -> (&S, Option<&HashMap<usize, A>>) {
+        let state = match self.nodes.last() {
+            Some(HistoryNode::State(state)) => Some(state),
+            _ => None,
+        };
+        let actions = match self.nodes.get(self.nodes.len() - 2) {
+            Some(HistoryNode::Actions(actions)) => Some(actions),
+            _ => None,
+        };
+        return (state.unwrap(), actions);
     }
 }
